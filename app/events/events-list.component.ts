31cdc6d8c90fd07bfbd7a880a6aa0f7d16cd01f1
dsullivan <drew.sullivan@hyland.com>
2018-02-23 14:51:32 -0500

    Added linking to events and broke html out into its own file

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'events-list',
-    templateUrl: './events-list.component.html'
+    templateUrl: 'app/events/events-list.component.html'
 })
 export class EventsListComponent {
     event = {
