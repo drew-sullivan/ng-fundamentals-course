27dad00a90a45d328f2bba03616ae8b866a4bdc3
dsullivan <drew.sullivan@hyland.com>
2018-02-26 11:48:39 -0500

    added event-detail and broke it out into html template and css

---
+++ app/events/events-list/event-details/event-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    templateUrl: '/app/events/events-list/event-details/event-details.component.html',
+    styleUrls: ['/app/events/events-list/event-details/event-details.component.css']
+})
+export class EventDetailComponent {
+
+}
