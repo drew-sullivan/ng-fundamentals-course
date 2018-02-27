9892e929df3b711b243e59068fd1e3684fddae7a
dsullivan <drew.sullivan@hyland.com>
2018-02-27 16:26:52 -0500

    Added validation for making first name begin with a letter

--- app/user/profile/profile.component.ts
+++ app/user/profile/profile.component.ts
@@ -17,7 +17,7 @@ export class ProfileComponent implements OnInit {
   constructor(private auth: AuthService, private router:Router) { }
     
   ngOnInit() {
-    this.firstName = new FormControl(this.auth.currentUser.firstName, Validators.required);
+    this.firstName = new FormControl(this.auth.currentUser.firstName, [Validators.required, Validators.pattern('[a-zA-Z].*')]);
     this.lastName = new FormControl(this.auth.currentUser.lastName, Validators.required);
     this.profileForm = new FormGroup({
       firstName: this.firstName,
