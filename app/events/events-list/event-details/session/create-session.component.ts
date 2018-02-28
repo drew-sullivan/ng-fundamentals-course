d8bf7ae21084759462bd9b854f0b013d1081be6c
dsullivan <drew.sullivan@hyland.com>
2018-02-28 11:23:25 -0500

    Added custom validator for restricted words into shared forder and imported it into create-session

--- app/events/events-list/event-details/session/create-session.component.ts
+++ app/events/events-list/event-details/session/create-session.component.ts
@@ -1,4 +1,4 @@
-import { ISession } from './../../../shared/event.model';
+import { ISession, restrictedWords } from './../../../shared/index';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
@@ -21,7 +21,7 @@ export class CreateSessionComponent implements OnInit {
         this.presenter = new FormControl('', reqd);
         this.duration = new FormControl('', reqd);
         this.level = new FormControl('', reqd);
-        this.abstract = new FormControl('', [reqd, Validators.maxLength(400)]);
+        this.abstract = new FormControl('', [reqd, Validators.maxLength(400), restrictedWords(['foo', 'bar'])]);
 
         this.newSessionForm = new FormGroup({
             name: this.name,
