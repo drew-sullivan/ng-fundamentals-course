c4ba2fa4f917eebcfa116e2eba6dd23f8a800ca3
dsullivan <drew.sullivan@hyland.com>
2018-03-02 09:35:44 -0500

    Made it to where the modal closes after making a selection using viewchild

--- app/common/simple-modal/simple-modal.component.ts
+++ app/common/simple-modal/simple-modal.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { JQ_TOKEN } from './../jQuery.service';
+import { Component, Input, ViewChild, ElementRef, Inject } from '@angular/core';
 
 @Component({
     selector: 'simple-modal',
@@ -9,4 +10,11 @@ export class SimpleModalComponent {
 
     @Input() title: string;
     @Input() elementId: string;
+    @ViewChild('modalContainer') containerEl: ElementRef;
+
+    constructor(@Inject(JQ_TOKEN) private $: any) { }
+
+    closeModal() {
+        this.$(this.containerEl.nativeElement).modal('hide');
+    }
 }
